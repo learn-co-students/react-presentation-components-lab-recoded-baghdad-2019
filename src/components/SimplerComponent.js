a175ad9f9dfc36267fe333ccfc25fae0f21910f9
dhuha <duha_9.com>
2019-12-06 17:00:38 +0300

    solved

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,7 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+let SimplerComponent = ({ handleClick }) =>
+    <div onClick={(event) => { handleClick(event) }} style={{ backgroundColor: "#fff" }}>I am just happy</div>
+
+export default SimplerComponent
--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(event) => event.target.style.backgroundColor === "rgb(255, 255, 255)" || undefined ?
+      event.target.style.backgroundColor = "#f72a2a" : event.target.style.backgroundColor = "#fff"} />
   </div>,
   document.getElementById('root')
 );
